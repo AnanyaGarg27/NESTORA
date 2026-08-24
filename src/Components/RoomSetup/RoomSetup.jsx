c54284317971ce84c495437ab2089ee1b3f62337
Nikita <nikita47khatri@gmail.com>
2026-08-24 14:51:58 +0530

    Add room setup component

---
+++ src/Components/RoomSetup/RoomSetup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function RoomSetup() {
+  return (
+    <div>
+      <h1>Room Designer</h1>
+      <p>Let's design your room.</p>
+    </div>
+  );
+}
+
+export default RoomSetup;
